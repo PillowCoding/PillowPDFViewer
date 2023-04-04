9ffc82f6d38900daa0aba65fad0b98ef5d502a15
PillowCoding <127198493+PillowCoding@users.noreply.github.com>
2023-04-04 17:17:15 +0200

    Remove unused ESLint error

--- src/pdfjs-viewer/projects/ng2-pdfjs-viewer/src/lib/templateRef.directive.ts
+++ src/pdfjs-viewer/projects/ng2-pdfjs-viewer/src/lib/templateRef.directive.ts
@@ -3,7 +3,6 @@ import { Directive, Input, TemplateRef } from "@angular/core";
 @Directive({ selector: '[libTemplateRef]' })
 export class templateRefDirective
 {
-    // eslint-disable-next-line @angular-eslint/no-input-rename
     @Input('libTemplateRef') type?: string;
     constructor(public template: TemplateRef<unknown>) {}
 }
