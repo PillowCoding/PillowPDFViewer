a407a1ffca6c890f7864e158d1442da66b185764
PillowCoding <127198493+PillowCoding@users.noreply.github.com>
2023-04-17 11:49:30 +0200

    Remove some code I missed

--- src/pdfjs-viewer/src/app/app.component.ts
+++ src/pdfjs-viewer/src/app/app.component.ts
@@ -2,16 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-root',
   template: `
-    <div class="main-container">
-      <div class="pdf-toolbar">
-        <a class="button margin-right-small" routerLink="/simple">Simple</a>
-        <a class="button margin-right-small" routerLink="/download">Download</a>
-        <a class="button margin-right-small" routerLink="/annotate">Annotate</a>
-        <a class="button margin-right-small" routerLink="/events">Events</a>
-        <a class="button" routerLink="/modified">Modified</a>
-      </div>
-      <router-outlet></router-outlet>
-    </div>
+    <router-outlet></router-outlet>
   `,
   styleUrls: ['app.component.scss']
 })
