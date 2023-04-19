b298d8413cc1ea01378ae2746952c5a3a26dfe43
PillowCoding <127198493+PillowCoding@users.noreply.github.com>
2023-04-19 11:59:27 +0200

    Added sidebar dragging

--- src/pdf-viewer/projects/ngx-pillow-pdf-viewer/src/lib/sidebar/pdf-sidebar.component.ts
+++ src/pdf-viewer/projects/ngx-pillow-pdf-viewer/src/lib/sidebar/pdf-sidebar.component.ts
@@ -6,4 +6,14 @@ import { Component } from "@angular/core";
     styleUrls: ['pdf-sidebar.component.scss', './../common.scss']
 })
 export class PdfSidebarComponent {
+    private _sidebarWidth = 32;
+
+    public get sidebarWidth()
+    {
+        return this._sidebarWidth;
+    }
+
+    public onSidebarWidthChanged(difference: number) {
+        this._sidebarWidth += difference;
+    }
 }
